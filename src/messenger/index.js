8680959d67d66ac233ba5c53e3bb31e104a43cac
Ayan Yenbekbay <ayan.yenb@gmail.com>
2016-10-07 03:52:38 +0600

    chore(eslint): make eslint configuration stricter

--- src/messenger/index.js
+++ src/messenger/index.js
@@ -1,5 +1,7 @@
 /* @flow */
 
+/* eslint-disable import/prefer-default-export */
+
 import createMessengerBot from './createMessengerBot';
 
 export { createMessengerBot };
--- src/telegram/index.js
+++ src/telegram/index.js
@@ -1,5 +1,7 @@
 /* @flow */
 
+/* eslint-disable import/prefer-default-export */
+
 import createTelegramBot from './createTelegramBot';
 
 export { createTelegramBot };
